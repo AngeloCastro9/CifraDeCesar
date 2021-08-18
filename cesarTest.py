ce830af62e7aba33422602fe60831b7749cabd32
Angelo <castromatheus060@gmail.com>
2021-08-18 20:22:38 -0300

    feat: BruteForce

--- cesarTest.py
+++ cesarTest.py
@@ -1,4 +1,4 @@
-def cesar(text, key):
+def cesar(text, key, isDecripted):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encriptedText = ''
     for later in text:
@@ -6,14 +6,22 @@ def cesar(text, key):
         if index == -1:
             encriptedText += later
         else:
-            newIndex = index + key
+            newIndex = index + key if isDecripted == True else index - key
             newIndex = newIndex % len(alphabet)
             encriptedText += alphabet[newIndex:newIndex+1]
     return encriptedText
 
 
+def bruteForce(encriptedText):
+    keys = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+    for key in keys:
+        key = int(key)
+        print('Key:', key, 'Result:', cesar(encriptedText, key, False))
+
 text, key = input("Digite o texto seguido da chave (separe por ,): ").strip().split(",")
 
 key = int(key)
 
-print(cesar(text.lower(), key))
+encripted = cesar(text.lower(), key, True)
+print('Encripted', encripted)
+bruteForce(encripted)
