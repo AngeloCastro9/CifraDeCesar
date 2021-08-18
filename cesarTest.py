fbf7ae026bab3e57479fa88124f344e238eecd4b
Angelo <castromatheus060@gmail.com>
2021-08-18 20:23:38 -0300

    feat: for range

--- cesarTest.py
+++ cesarTest.py
@@ -13,8 +13,7 @@ def cesar(text, key, isDecripted):
 
 
 def bruteForce(encriptedText):
-    keys = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
-    for key in keys:
+    for key in range(26):
         key = int(key)
         print('Key:', key, 'Result:', cesar(encriptedText, key, False))
 
